573130a85396c533fdcd7b73853dd358cbb90c46
oggiarthur <arthur.prakh@gmail.com>
2020-03-27 14:18:58 +0200

    first commit

---
+++ lesson1/main_lesson.js
@@ -0,0 +1,143 @@
+// console.log("Hello world");
+
+// string
+// number
+// boolean
+// undefined
+// null
+// object
+
+// var name = 'Ivan';
+// name = 35345;
+
+// console.log(x);
+// var x = 10;
+
+// console.log(x);
+// let x = 10;
+
+// const x = 10;
+// x = 20;
+
+// let word1 = "Hello";
+// let word2 = " world";
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+// console.log(word1 + b);
+// console.log(a - b);
+// console.log(word1 - word2);
+// console.log(word1 - b);
+// console.log(word1 * b);
+// console.log(word1 / b);
+
+// let x = 10;
+// let y = "20";
+// console.log(x + +y);
+
+// let a = 9;
+// a++;
+// a = a + 1;
+// console.log(a);
+// console.log(a++);
+// console.log(a);
+
+// console.log(a);
+// console.log(--a);
+// console.log(a);
+
+// let x = 10;
+// // x = x + 5;
+// x += 5;
+// console.log(x);
+
+// console.log(2 > 2);
+// console.log(2 <= 2);
+// console.log(2 == 2);
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log("a" < "z");
+// console.log("Ab" < "Ac");
+// console.log("Acsdfsdf" > "Ac");
+// console.log("a" > "A");
+// console.log(undefined === 0);
+
+// let age = 30;
+
+// if (age >= 18) {
+//   console.log("держи пиво");
+// } else {
+//   console.log("приходи с папой");
+// }
+
+// let x = "false";
+// false - false, 0, null, undefined, "";
+
+// if (x) {
+//   console.log("xxxxx");
+// }
+
+// alert("Hello world");
+// let x = confirm("???");
+// let x = prompt("???");
+// console.log(x);
+
+// let age = prompt("Сколько лет ??");
+// // console.log(age);
+
+// // console.log(NaN === NaN);
+
+// if (age === null || !+age) {
+//   console.log("Пока!");
+// } else {
+//   if (+age >= 18) {
+//     console.log("держи пиво");
+//   } else {
+//     console.log("приходи с папой");
+//   }
+// }
+
+// let x = true;
+// console.log(!x);
+
+// let x = 10;
+// let y = 10;
+// // console.log(x == y);
+// // console.log(x != y);
+// console.log(x === y);
+// console.log(x !== y);
+
+// &&
+// false false = flase
+// false true = false
+// true true = true
+
+// ||
+// false false = flase
+// false true = true
+// true true = true
+
+// let x = true;
+// let y = "sdfsdfsd";
+// // console.log(!!y);
+
+// console.log(x === !!y);
+
+// Спрашивать (prompt) у пользователя размер памяти телефона
+// и вывоедите (alert/document.write()) соответствующую цену
+
+// 64GB - 300$
+// 128GB - 500$
+// 256GB - 700$
+
+// Спрашивать у пользователя цвет телефона
+// и показывать соответствующую картинку
+
+// let price = 0;
+// if (memory === 64) {
+//     price = 300;
+// }
+
+// alert(price)
