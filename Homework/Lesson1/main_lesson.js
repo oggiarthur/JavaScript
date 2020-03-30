794fd8fc0b53ff79c4e937d6ceece892bafb69e1
Arthur <arthur.prakh@gmail.com>
2020-03-30 16:38:57 +0300

    Task 1 modified and Task 2 done

--- Homework/Lesson1/main_lesson.js
+++ Homework/Lesson1/main_lesson.js
@@ -1,41 +1,59 @@
+
+
+// 1 ЗАДАНИЕ
+
 // Спрашивать (prompt) у пользователя размер памяти телефона
 // и вывоедите (alert/document.write()) соответствующую цену
 
 // 64GB - 300$
 // 128GB - 500$
 // 256GB - 700$
-for (let counter = 0; counter <15; counter++){
-let mem = prompt ("Какой размер памяти Вам нужен? 64GB 128GB 256GB");
-console.log (mem);
-console.log(NaN === NaN);
-if (mem === null || !+mem) {
-  console.log("Неккоректный ввод");
 
-} else {
-  if (+mem == 64 ) {
-    alert("300$");
-    break;
-  } else if (+mem== 128) {
-    alert("500$");
-    break;
-  } else if (+mem== 256) {
-    alert("700$");
-    break;
-  } else {
-    alert("выберите один из 3 вариантов 64, 128, 256"); 
-  }
-}
-}
 
+// for (let counter = 0; counter <15; counter++){
+// let mem = prompt ("Какой размер памяти Вам нужен? 64 или 128 или 256", " ");
+// console.log (mem);
+// console.log(NaN === NaN);
+// if (mem === null || !+mem) {
+//   console.log("Неккоректный ввод");
+//   alert("Вы ничего не ввели или ввели строку ");
+//   continue;
+// } else {
+//   if (+mem == 64 ) {
+//     alert("300$");
+//     break;
+//   } else if (+mem== 128) {
+//     alert("500$");
+//     break;
+//   } else if (+mem== 256) {
+//     alert("700$");
+//     break;
+//   } else {
+//     alert("выберите один из 3 вариантов 64, 128, 256"); 
+//   }
+// }
+// }
+
+
+// 2 ЗАДАНИЕ
 
 // Спрашивать у пользователя цвет телефона
 // и показывать соответствующую картинку
 
-// let price = 0;
-// if (memory === 64) {
-//     price = 300;
-// }
 
-// alert(price)
-
-
+// let color = prompt ("Какой цвет телефона вас интересует? \n    \nБелый(White) \nЧерный(Black) \nЗеленый(Green) \nКрасный(Red) \nЖелтый(Yellow) \nПурпурный(Purple)",);
+// if (color==="Белый"  || color==="White"){
+// document.write("<img src='img/White.png' alt='' ");
+//     } else if (color==="Черный" || color==="Black"){
+//     document.write("<img src='img/Black.png' alt='' ");
+//       } else if (color==="Зеленый" || color==="Green"){
+//     document.write("<img src='img/Green.png' alt='' ");
+//         } else if (color==="Красный" || color==="Red"){
+//     document.write("<img src='img/Red.png' alt='' ");
+//         } else if (color==="Желтый" || color==="Yellow"){
+//     document.write("<img src='img/Yellow.png' alt='' ");
+//         }  else if (color==="Пурпурный" || color==="Purple"){
+//     document.write("<img src='img/Purple.png' alt='' ");
+//         } else{
+//     document.write("У нас нет такого цвета");
+//     }
